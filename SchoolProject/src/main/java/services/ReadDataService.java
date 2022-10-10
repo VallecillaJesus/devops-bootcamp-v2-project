1543d6ff28b64ba99e02617ffcba10e57470e304
Jesus Vallecilla <jesusvallecilla27@gmail.com>
2022-10-10 00:27:34 -0500

    refactor: refactor methods

--- SchoolProject/src/main/java/services/ReadDataService.java
+++ SchoolProject/src/main/java/services/ReadDataService.java
@@ -3,6 +3,7 @@ package services;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import models.Student;
+import models.Teacher;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -30,5 +31,24 @@ public class ReadDataService {
         }
 
         return students;
+    }
+
+    public List<Teacher> readTeachersDataFromJson() {
+        List<Teacher> teachers = null;
+
+        try {
+            Gson gson = new Gson();
+            var path = (Paths.get("teachers.json"));
+            System.out.println(path);
+            Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/teachers.json"));
+
+            teachers = gson.fromJson(reader, new TypeToken<List<Teacher>>() {}.getType());
+            reader.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return teachers;
     }
 }
--- SchoolProject/src/main/java/services/WriteDataService.java
+++ SchoolProject/src/main/java/services/WriteDataService.java
@@ -1,18 +1,26 @@
 package services;
 
 import models.Student;
+import models.Teacher;
 import repositories.StudentRepository;
+import repositories.TeacherRepository;
 
 import java.util.List;
 
 public class WriteDataService {
     private StudentRepository studentRepository;
+    private TeacherRepository teacherRepository;
 
-    public WriteDataService(StudentRepository studentRepository) {
+    public WriteDataService(StudentRepository studentRepository, TeacherRepository teacherRepository) {
         this.studentRepository = studentRepository;
+        this.teacherRepository = teacherRepository;
     }
 
     public void writeStudentsDataToRepository(List<Student> students) {
-        students.forEach(student -> this.studentRepository.addStudent(student));
+        students.forEach(student -> this.studentRepository.save(student));
+    }
+
+    public void writeTeachersDataToRepository(List<Teacher> teachers) {
+        teachers.forEach(teacher -> this.teacherRepository.save(teacher));
     }
 }
