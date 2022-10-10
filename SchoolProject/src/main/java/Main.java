fb34c03875842cc4cc24dae58871a02028f522de
Jesus Vallecilla <jesusvallecilla27@gmail.com>
2022-10-10 00:21:56 -0500

    refactor(Main.java): remove logic

--- SchoolProject/src/main/java/Main.java
+++ SchoolProject/src/main/java/Main.java
@@ -1,54 +1,16 @@
 import models.School;
+import models.Student;
+import models.Teacher;
 import repositories.StudentRepository;
-import services.AccessValidator;
+import helpers.AccessValidator;
+import repositories.TeacherRepository;
 import services.ReadDataService;
 import services.WriteDataService;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String args[]) {
-        StudentRepository studentRepository = new StudentRepository();
-
-        ReadDataService readDataService = new ReadDataService();
-        var students = readDataService.readStudentsDataFromJson();
-        WriteDataService writeDataService = new WriteDataService(studentRepository);
-        writeDataService.writeStudentsDataToRepository(students);
-
-        Scanner scanner = new Scanner(System.in);
-        var school = new School("American School");
-
-        System.out.println("=============================");
-        System.out.println("Welcome to " + school.getName());
-        System.out.println("=============================");
-
-        System.out.println("Select your role please: ");
-        System.out.println("1. Teacher");
-        System.out.println("2. Student");
-
-        String option = scanner.nextLine();
-        boolean access = false;
-
-        while (!access) {
-            System.out.println("Please enter your code");
-            AccessValidator accessValidator = new AccessValidator(studentRepository);
-
-            String code = scanner.nextLine();
-
-            switch (option) {
-                case "1":
-                    var teacher = accessValidator.verifyTeacherAccess(code);
-                    if (teacher != null) {
-                        access = true;
-                        break;
-                    }
-                case "2":
-                    var student = accessValidator.verifyStudentAccess(code);
-                    if (student != null) {
-                        access = true;
-                        break;
-                    }
-            }
-        }
+public class  Main {
+    public static void main(String args[]) throws Exception {
+        ApplicationManager.start();
     }
 }
